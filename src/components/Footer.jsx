d2ad6f02d0b6a918eb52026d24b942d92522704d
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2025-03-29 08:47:36 +0530

    bug: fixed footer responsively

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,38 +2,47 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { navLinks } from "../../data/navLinks";
-import EventLogo from '/logo_.svg';
+import EventLogo from "/logo_.svg";
 
 const Footer = () => {
   return (
-    <footer id="contact" className="bg-zinc-900 px-5">
-      <div className="px-20 py-5 border-b flex items-center justify-between">
-        <img className="w-60" src={EventLogo} alt="" />
-        <span className="flex gap-5 text-lg">
-          {navLinks.map((link,i) => (
-            <a key={i} className="hover:text-[#13D0F4]" href={link.path}>
-              <p>{link.text}</p>
-            </a>
-          ))}
-        </span>
-        <span>
-          <span className="flex gap-3 items-center py-1">
-            <Link
-              className="hover:text-[#13D0F4]"
-              to={"https://www.instagram.com/cmrit_illuminate"}
-              target="_blank_"
-            >
-              <FaInstagram size={30} />
-            </Link>
-            <Link
-              className="hover:text-[#13D0F4]"
-              to={"https://www.linkedin.com/company/cmritilluminate/"}
-              target="_blank_"
-            >
-              <FaLinkedin size={30} />
-            </Link>
-          </span>
-        </span>
+    <footer className="bg-zinc-950 text-white xl:px-10 py-5 px-5">
+      <div className="border-white/80 border-b-2">
+        <div className="xl:flex justify-between items-start xl:px-15">
+          <div className="xl:w-2/5">
+            <img src="/logo.png" alt="Logo" className="h-20" />
+            <p className="text-sm xl:text-base">
+              Illuminate 2K25: CMR Institute of Technology's premier tech fest,
+              inspiring innovation and tech excellence.
+            </p>
+          </div>
+          <div className="py-5">
+            {navLinks.map((ele, id) => (
+              <a key={id} href={ele.path} className="hover:text-[#13D0F4]">
+                <p className="pb-1">{ele.text}</p>
+              </a>
+            ))}
+          </div>
+          <div className="py-2">
+            <h1>Contact us</h1>
+            <div className="flex text-3xl py-3 gap-3">
+              <Link
+                className="hover:text-[#13D0F4]"
+                to={"https://www.instagram.com/cmrit_illuminate"}
+                target="_blank_"
+              >
+                <FaInstagram />
+              </Link>
+              <Link
+                className="hover:text-[#13D0F4]"
+                to={"https://www.linkedin.com/company/cmritilluminate/"}
+                target="_blank_"
+              >
+                <FaLinkedin />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
       <h1 className="text-center text-lg py-3">
         © Copyright Illuminate 2025 CMRIT. All rights reserved.
